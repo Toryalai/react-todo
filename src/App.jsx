66e155b4ff2982ac2cc9920d5919ff9d18761ef6
toryalai <toryalai.helali@gmail.com>
2024-06-25 22:10:46 -0400

    Second Assignment.

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import TodoList from './TodoList';
+import AddTodoForm from './AddTodoForm';
 
-const todoList = [
-  { id: 1, title: "Complete assignment 1" },
-  { id: 2, title: "Complete assignment 2" },
-  { id: 3, title: "Complete assignment 3" }
-];
 
 function App() {
 
   return (
     <div className="App">
       <h1>Todo List</h1>
-      <ul>
-        {todoList.map(function(item){
-          return <li key={item.id}>{item.title}</li>;
-        })}
-      </ul>
+      <AddTodoForm />
+      <TodoList />
     </div>
   );
 }
---
+++ src/TodoList.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const todoList = [
+  { id: 1, title: "Complete assignment 1" },
+  { id: 2, title: "Complete assignment 2" },
+  { id: 3, title: "Complete assignment 3" }
+];
+
+function TodoList() {
+  return (
+    <ul>
+      {todoList.map(function(item) {
+        return <li key={item.id}>{item.title}</li>;
+      })}
+    </ul>
+  );
+}
+
+export default TodoList;
