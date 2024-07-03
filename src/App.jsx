c2e8b18c40bf81bcc954628813ae39fd0af3fb93
toryalai <toryalai.helali@gmail.com>
2024-07-02 22:34:45 -0400

    Lesson_1_3_Props State and Handlers

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import TodoList from './TodoList';
-import AddTodoForm from './AddTodoForm';
-
+import React, { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+import TodoList from './TodoList'; 
+import AddTodoForm from './AddTodoForm'; 
 
 function App() {
+  const [newTodo, setNewTodo] = useState('');
 
   return (
     <div className="App">
       <h1>Todo List</h1>
-      <AddTodoForm />
-      <TodoList />
+      <AddTodoForm onAddTodo={setNewTodo} /> 
+      <p>{newTodo}</p> 
+      <TodoList /> 
     </div>
   );
 }
 
-export default App
+export default App;
--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import TodoListItem from './TodoListItem'; 
 
 const todoList = [
   { id: 1, title: "Complete assignment 1" },
@@ -9,8 +10,8 @@ const todoList = [
 function TodoList() {
   return (
     <ul>
-      {todoList.map(function(item) {
-        return <li key={item.id}>{item.title}</li>;
+      {todoList.map(function(todo) {
+        return <TodoListItem key={todo.id} todo={todo} />; 
       })}
     </ul>
   );
